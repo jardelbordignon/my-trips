90228fbefbb489b4fb7b06b006c7b6d0db680ec2
Jardel <jardel1101@gmail.com>
2022-01-09 23:34:06 -0300

    Adicionando css variables

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,6 +1,22 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+:root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eee;
+
+    --container: 1000rem;
+    --small: 15rem;
+    --medium: 30rem;
+    --large: 50rem;
+  }
+
+  [data-theme='dark'] {
+    --highlight: #030518;
+    --background: #e20e8d;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -19,10 +35,26 @@ export default createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
+    background: var(--background);
+    color: var(--white);
   }
 
   body, input, textarea, select, button {
     font-family: -apple-system, BlinkMacSystemFont, Roboto, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 16rem;
+  }
+
+  p {
+    font-size: 20rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
+    text-decoration: none;
+    &:hover {
+      transition: opacity 0.2s;
+      opacity: 0.8;
+    }
   }
 `
